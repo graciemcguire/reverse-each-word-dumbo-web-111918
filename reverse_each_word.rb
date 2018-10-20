67ede57eaeacc1e09253bd5e832ceb61982446fd
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-20 14:31:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  returnArr = []
+  array.each do |word|
+    returnArr << word.reverse
+  end
+  returnArr.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  returnArr = []
+  array.collect do |word|
+    returnArr << word.reverse
+  end
+  returnArr.join(" ")
+end
